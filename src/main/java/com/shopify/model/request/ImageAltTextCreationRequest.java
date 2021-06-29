9843bd0e99ced0496bd377caacf62d882862c090
Wang Zhengdong <zhengdong.wang@bloomchic.tech>
2021-06-29 18:33:58 +0800

    修复访问修饰

--- src/main/java/com/shopify/model/request/ImageAltTextCreationRequest.java
+++ src/main/java/com/shopify/model/request/ImageAltTextCreationRequest.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class ImageAltTextCreationRequest {
 
-	static final String KEY = "alt";
-	static final String NAMESPACE = "tags";
-	static final MetafieldValueType VALUE_TYPE = MetafieldValueType.STRING;
+	public static final String KEY = "alt";
+	public static final String NAMESPACE = "tags";
+	public static final MetafieldValueType VALUE_TYPE = MetafieldValueType.STRING;
 
 	public static interface ImageAltTextStep {
 		public BuildStep withImageAltText(final String imageAltText);
