9eb827d331a75f97deb6da430235d280056628b1
Wang Zhengdong <zhengdong.wang@bloomchic.tech>
2021-06-30 16:19:06 +0800

    增加接口模型

---
+++ src/main/java/com/shopify/model/roots/ShopifyDeprecatedApiCallsRoot.java
@@ -0,0 +1,40 @@
+package com.shopify.model.roots;
+
+import com.shopify.model.structs.ShopifyDeprecatedApiCall;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+public class ShopifyDeprecatedApiCallsRoot {
+
+    @XmlElement(name = "data_updated_at")
+    private String dataUpdatedAt;
+    @XmlElement(name = "deprecated_api_calls")
+    private List<ShopifyDeprecatedApiCall> deprecatedApiCalls;
+
+    public String getDataUpdatedAt() {
+        return dataUpdatedAt;
+    }
+
+    public void setDataUpdatedAt(String dataUpdatedAt) {
+        this.dataUpdatedAt = dataUpdatedAt;
+    }
+
+    public List<ShopifyDeprecatedApiCall> getDeprecatedApiCalls() {
+        return deprecatedApiCalls;
+    }
+
+    public void setDeprecatedApiCalls(List<ShopifyDeprecatedApiCall> deprecatedApiCalls) {
+        this.deprecatedApiCalls = deprecatedApiCalls;
+    }
+
+    @Override
+    public String toString() {
+        return "ShopifyDeprecatedApiCallsRoot{" +
+                "dataUpdatedAt='" + dataUpdatedAt + '\'' +
+                ", deprecatedApiCalls=" + deprecatedApiCalls +
+                '}';
+    }
+}
---
+++ src/main/java/com/shopify/model/structs/ShopifyDeprecatedApiCall.java
@@ -0,0 +1,102 @@
+package com.shopify.model.structs;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ShopifyDeprecatedApiCall {
+    @XmlElement(name = "api_type")
+    private String apiType;
+    private String description;
+    @XmlElement(name = "documentation_url")
+    private String documentationUrl;
+    private String endpoint;
+    @XmlElement(name = "last_call_at")
+    private String lastCallAt;
+    @XmlElement(name = "migration_deadline")
+    private String migrationDeadline;
+    @XmlElement(name = "graphql_schema_name")
+    private String graphqlSchemaName;
+    private String version;
+
+    public String getApiType() {
+        return apiType;
+    }
+
+    public void setApiType(String apiType) {
+        this.apiType = apiType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDocumentationUrl() {
+        return documentationUrl;
+    }
+
+    public void setDocumentationUrl(String documentationUrl) {
+        this.documentationUrl = documentationUrl;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getLastCallAt() {
+        return lastCallAt;
+    }
+
+    public void setLastCallAt(String lastCallAt) {
+        this.lastCallAt = lastCallAt;
+    }
+
+    public String getMigrationDeadline() {
+        return migrationDeadline;
+    }
+
+    public void setMigrationDeadline(String migrationDeadline) {
+        this.migrationDeadline = migrationDeadline;
+    }
+
+    public String getGraphqlSchemaName() {
+        return graphqlSchemaName;
+    }
+
+    public void setGraphqlSchemaName(String graphqlSchemaName) {
+        this.graphqlSchemaName = graphqlSchemaName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "ShopifyDeprecatedApiCall{" +
+                "apiType='" + apiType + '\'' +
+                ", description='" + description + '\'' +
+                ", documentationUrl='" + documentationUrl + '\'' +
+                ", endpoint='" + endpoint + '\'' +
+                ", lastCallAt='" + lastCallAt + '\'' +
+                ", migrationDeadline='" + migrationDeadline + '\'' +
+                ", graphqlSchemaName='" + graphqlSchemaName + '\'' +
+                ", version='" + version + '\'' +
+                '}';
+    }
+}
