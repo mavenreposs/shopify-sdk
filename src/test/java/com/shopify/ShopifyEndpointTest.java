4972b8723db98027f7268abcc2a019ef13ae4c77
Wang Zhengdong <zhengdong.wang@bloomchic.tech>
2021-06-30 15:34:30 +0800

    add unit test

--- src/test/java/com/shopify/ShopifyEndpointTest.java
+++ src/test/java/com/shopify/ShopifyEndpointTest.java
@@ -1,12 +1,49 @@
 package com.shopify;
 
+import com.shopify.model.ShopifyPage;
+import com.shopify.model.structs.ShopifyProduct;
+import org.junit.Before;
 import org.junit.Test;
+
+import javax.ws.rs.client.WebTarget;
+import java.util.concurrent.TimeUnit;
 
 public class ShopifyEndpointTest {
 
+    private static final String SHOP_SUBDOMAIN = System.getenv("SHOP_SUBDOMAIN");
+    private static final String ACCESS_TOKEN = System.getenv("ACCESS_TOKEN");
+
+    private ShopifySdk shopifySdk;
+
+    @Before
+    public void setUp() {
+        shopifySdk = ShopifySdk.newBuilder()
+                .withSubdomain(SHOP_SUBDOMAIN)
+                .withAccessToken(ACCESS_TOKEN)
+                .withMaximumRequestRetryTimeout(5, TimeUnit.SECONDS)
+                .withMaximumRequestRetryRandomDelay(5, TimeUnit.SECONDS)
+                .withApiVersion("2021-04")
+                .build();
+    }
+
     @Test
-    public void testOrder() {
-        System.out.println("test");
+    public void testShopifySdk() {
+        System.out.println(shopifySdk);
+    }
+
+    @Test
+    public void getProducts() {
+        final ShopifyPage<ShopifyProduct> shopifyProducts = shopifySdk.getProducts(null, 1);
+        System.out.println(shopifyProducts);
+    }
+
+    @Test
+    public void getUrls() {
+        final WebTarget productsUrl = shopifySdk.getProductsUrl(null, 1);
+        System.out.println(productsUrl);
+
+        final WebTarget ordersUrl = shopifySdk.getOrdersUrl(1);
+        System.out.println(ordersUrl);
     }
 
 }
