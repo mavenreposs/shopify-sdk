c1e2c6cc3b494996fcbf5b744e3713e54053e0f6
Wang Zhengdong <zhengdong.wang@bloomchic.tech>
2021-07-24 18:21:50 +0800

    补充实体类和字段

---
+++ src/main/java/com/shopify/model/structs/ShopifyDiscountCode.java
@@ -0,0 +1,55 @@
+package com.shopify.model.structs;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created with IntelliJ IDEA.
+ * ClassName ShopifyDiscountCode
+ * Create by zhengdong.wang@bloomchic.tech
+ * Date 2021/7/24 18:20
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ShopifyDiscountCode {
+    private String code;
+    private String amount;
+    private String type;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "ShopifyDiscountCode{" +
+                "code='" + code + '\'' +
+                ", amount='" + amount + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
