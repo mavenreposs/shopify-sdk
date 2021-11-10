6288a00586f2b77be85b4f7f0cd6bc3be16582a6
Wang Zhengdong <zhengdong.wang@bloomchic.tech>
2021-11-10 11:26:25 +0800

    实现接口请求逻辑

--- src/main/java/com/shopify/actions/InventoryItemAction.java
+++ src/main/java/com/shopify/actions/InventoryItemAction.java
@@ -1,4 +1,14 @@
 package com.shopify.actions;
 
+import com.shopify.model.structs.ShopifyInventoryItem;
+
 public interface InventoryItemAction {
+
+    ShopifyInventoryItem getInventoryItem(final String inventoryItemId);
+
+    ShopifyInventoryItem updateInventoryItem(final ShopifyInventoryItem shopifyInventoryItem);
+
+    ShopifyInventoryItem updateInventoryItem(final String inventoryItemId, boolean requiresShipping);
+
+
 }
---
+++ src/main/java/com/shopify/actions/impl/InventoryItemActionImpl.java
@@ -0,0 +1,77 @@
+package com.shopify.actions.impl;
+
+import com.shopify.ShopifyEndpoint;
+import com.shopify.ShopifySdk;
+import com.shopify.actions.InventoryItemAction;
+import com.shopify.model.roots.ShopifyInventoryItemRoot;
+import com.shopify.model.structs.ShopifyInventoryItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * ClassName InventoryItemActionImpl
+ *
+ * @author Zhengdong Wang
+ * @version jdk11
+ * @CreateDate 2021/11/10 10:58
+ */
+public class InventoryItemActionImpl implements InventoryItemAction {
+
+    private final ShopifySdk shopifySdk;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductActionImpl.class);
+
+    public InventoryItemActionImpl(ShopifySdk shopifySdk) {
+        this.shopifySdk = shopifySdk;
+    }
+
+
+    @Override
+    public ShopifyInventoryItem getInventoryItem(String inventoryItemId) {
+        final Response response = shopifySdk.getShopifyWebTarget().get(
+                        shopifySdk.getWebTarget()
+                                .path(ShopifyEndpoint.INVENTORY_ITEMS)
+                                .path(inventoryItemId)
+                );
+        final ShopifyInventoryItemRoot shopifyInventoryItemRootResponse = response.readEntity(ShopifyInventoryItemRoot.class);
+        return shopifyInventoryItemRootResponse.getInventoryItem();
+    }
+
+    @Override
+    public ShopifyInventoryItem updateInventoryItem(ShopifyInventoryItem shopifyInventoryItem) {
+        final ShopifyInventoryItemRoot shopifyInventoryItemRootRequest = new ShopifyInventoryItemRoot();
+        shopifyInventoryItem.setCreatedAt(null);
+        shopifyInventoryItem.setUpdatedAt(null);
+        shopifyInventoryItemRootRequest.setInventoryItem(shopifyInventoryItem);
+        final Response response = shopifySdk.getShopifyWebTarget()
+                .put(
+                        shopifySdk.getWebTarget()
+                                .path(ShopifyEndpoint.INVENTORY_ITEMS).path(shopifyInventoryItem.getId()),
+                        shopifyInventoryItemRootRequest
+                );
+        final ShopifyInventoryItemRoot shopifyInventoryItemRootResponse = response.readEntity(ShopifyInventoryItemRoot.class);
+        final ShopifyInventoryItem updatedShopifyInventoryItem = shopifyInventoryItemRootResponse.getInventoryItem();
+        return updatedShopifyInventoryItem;
+    }
+
+    @Override
+    public ShopifyInventoryItem updateInventoryItem(String inventoryItemId, boolean requiresShipping) {
+        final ShopifyInventoryItemRoot shopifyInventoryItemRootRequest = new ShopifyInventoryItemRoot();
+        final ShopifyInventoryItem shopifyInventoryItem = new ShopifyInventoryItem();
+        shopifyInventoryItem.setId(inventoryItemId);
+        shopifyInventoryItem.setRequiresShipping(requiresShipping);
+        shopifyInventoryItemRootRequest.setInventoryItem(shopifyInventoryItem);
+        final Response response = shopifySdk.getShopifyWebTarget()
+                .put(
+                        shopifySdk.getWebTarget()
+                                .path(ShopifyEndpoint.INVENTORY_ITEMS).path(shopifyInventoryItem.getId()),
+                        shopifyInventoryItemRootRequest
+                );
+        final ShopifyInventoryItemRoot shopifyInventoryItemRootResponse = response.readEntity(ShopifyInventoryItemRoot.class);
+        final ShopifyInventoryItem updatedShopifyInventoryItem = shopifyInventoryItemRootResponse.getInventoryItem();
+        return updatedShopifyInventoryItem;
+    }
+
+}
